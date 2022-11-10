7e6ae1be9c477d2210d0b8ecb6e2be9d529a80e1
Ryan Turner <RyanTurner02@protonmail.com>
2022-11-10 11:54:19 -0500

    Add support for cli arguments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,45 @@
+import org.apache.commons.cli.*;
 
 public class App {
     public static void main(String[] args) {
-        String albumURL = args[0];
+        Options options = new Options();
+        options.addOption("h", "Show this help message and exit");
+        options.addOption("c", "Display a counter next to each song when downloading");
+        options.addOption("i", "Add an index number to the file name of each song when downloading");
+
+        CommandLineParser defaultParser = new DefaultParser();
+        String albumURL = "";
+        boolean countOption = false;
+        boolean indexOption = false;
+
+        try {
+            CommandLine cmd = defaultParser.parse(options, args);
+
+            if (cmd.hasOption("h") || args.length == 0) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("khi-dl.jar [-h] [-c] [-i] [albumURL]", options);
+                System.exit(0);
+            }
+
+            if (cmd.hasOption("c")) {
+                countOption = true;
+            }
+
+            if (cmd.hasOption("i")) {
+                indexOption = true;
+            }
+
+            // check if the user entered the album url last
+            albumURL = args[args.length - 1];
+
+            if (!albumURL.startsWith("https://downloads.khinsider.com/")) {
+                System.out.println("Invalid Album URL");
+                System.exit(1);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
         Scraper scraper = new Scraper(albumURL);
         scraper.getSongs();
     }
